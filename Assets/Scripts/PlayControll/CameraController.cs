3eeff543529ffbed04f4d19126e5e19e6898290c
etihw_lin <etihw_mxlin@163.com>
2020-10-11 21:51:02 +0800

    1、角色控制 2、相机控制

---
+++ Assets/Scripts/PlayControll/CameraController.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public bool isMove = false; //可否移动相机
+
+    public float Xboundary = 200;
+    public float Zboundary = 200;
+
+    private int SightDistancespeed = 15;
+    private bool CameraIsLock = false; //相机是否锁定  
+    public float LockDistanceZ = 5f; //相机锁定时距离物体的位置
+    public float LockDistanceX = 0f; //相机锁定时距离物体的位置
+    private float RectSize = 50f; //矩形大小  
+    public float CameraMoveSpeed = 1f; //相机移动速度  
+    private Transform Player; //player的Transform  
+    private float CamerafieldOfView = 60; //相机的锥形视野范围  
+    private Camera camera; //相机  
+    private float a= 0.05f ;//倍数
+
+    //屏幕边缘四个矩形  
+    private Rect RectUp;
+    private Rect RectDown;
+    private Rect RectLeft;
+    private Rect RectRight;
+    void Start()
+    {
+        //实例化屏幕边缘的四个矩形出来  
+        RectUp = new Rect(0, Screen.height - RectSize, Screen.width, Screen.height);
+        RectDown = new Rect(0, 0, Screen.width, RectSize);
+        RectLeft = new Rect(0, 0, RectSize, Screen.width);
+        RectRight = new Rect(Screen.width - RectSize, 0, Screen.width, Screen.height);
+
+
+        //在场景内查找Tag为Player的物体  
+        Player = GameObject.FindGameObjectWithTag("Player").transform;  
+           
+          
+        camera = this.GetComponent<Camera>();
+
+        CameraMoveSpeed = CameraMoveSpeed * a;
+
+
+    }
+
+
+void Update()
+{
+
+    if (!isMove)
+        {
+            CameraIsLock = true;
+        }
+    //如果按下Y键锁定相机再次按下解锁。  
+    else if (Input.GetKeyDown(KeyCode.Y))
+    {
+        CameraIsLock = !CameraIsLock;
+    }
+
+
+    CameraMoveAndLock();//视角移动和锁定  
+    SightDistance();//视距的缩放  
+
+}
+//视角移动  
+void CameraMoveAndLock()
+{
+    ////空格回到自己  
+    //if (Input.GetKey(KeyCode.Space))
+    //{
+    //    transform.position = new Vector3(Player.position.x, transform.position.y, Player.position.z - 5f);
+    //}
+    //如果没有锁定相机（视野）可以移动  
+    if (CameraIsLock == false)
+    {
+        //如果鼠标在屏幕上的位置包含在这个矩形里  
+        if (RectUp.Contains(Input.mousePosition))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + CameraMoveSpeed);
+        }
+        if (RectDown.Contains(Input.mousePosition))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - CameraMoveSpeed);
+        }
+        if (RectLeft.Contains(Input.mousePosition))
+        {
+            transform.position = new Vector3(transform.position.x - CameraMoveSpeed, transform.position.y, transform.position.z);
+        }
+        if (RectRight.Contains(Input.mousePosition))
+        {
+            transform.position = new Vector3(transform.position.x + CameraMoveSpeed, transform.position.y, transform.position.z);
+        }
+        //判断相机移动的边缘在哪里，不能 超过设定的最远距离  
+        if (transform.position.z >= Zboundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Zboundary);
+        }
+        if (transform.position.z <= -Zboundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -Zboundary);
+        }
+        if (transform.position.x >= Xboundary)
+        {
+            transform.position = new Vector3(Xboundary, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= -Xboundary)
+        {
+            transform.position = new Vector3(-Xboundary, transform.position.y, transform.position.z);
+        }
+    }
+    else if (CameraIsLock == true)
+    {
+        //如果锁定视角，相机视野显示主角  
+        transform.position = new Vector3(Player.position.x - LockDistanceX, transform.position.y, Player.position.z - LockDistanceZ);
+    }
+
+}
+
+
+//中键滑轮拉远拉近  
+void SightDistance()
+{
+    float MouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
+    CamerafieldOfView += MouseScrollWheel * -SightDistancespeed;
+    CamerafieldOfView = Mathf.Clamp(CamerafieldOfView, 30, 60);
+    camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, CamerafieldOfView, Time.deltaTime * SightDistancespeed);
+
+}  
+} 
---
+++ Assets/Scripts/PlayControll/playController.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class playController : MonoBehaviour
+{
+    public float moveSpeed = 0;
+    public float roatSpeed = 0;
+    public float jumpSpeed = 0;
+    public float runSpeed = 0;
+    public float gravity = 0;
+    public string maskName = "Ground"; //输入地面层的Layer名字
+    public float dashDuration;// 控制冲刺时间
+    public float dashSpeed;// 冲刺速度
+    private bool isDash = false;
+    private float dashTime; //临时变量
+    private float dashCoefficient = 1;
+    public float dashCD;// 冲刺CD
+    private float dashCDtime = 0;// 临时变量
+
+    private CharacterController controller;
+
+    private Vector3 moveDirection = Vector3.zero;//角色移动
+
+    private int groundLayerIndex; //地面层
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        groundLayerIndex = LayerMask.GetMask(maskName); //初始化地面layer的序列
+
+        dashTime = dashDuration;//初始化冲刺时间
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * moveSpeed;//向前移动
+        //Vector3 move = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")) * moveSpeed;//移动位置
+        //controller.Move(move * Time.deltaTime); //移动
+        //transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * roatSpeed * Time.deltaTime, 0));//旋转
+
+
+        this.Rotating(); //角色旋转-朝向鼠标
+
+        if (dashCDtime <= 0 && Input.GetButtonDown("Jump"))
+        {
+            isDash = true;
+            moveDirection = transform.forward;// forward 指向物体当前的前方
+            moveDirection.y = 0f;// 只做平面的上下移动和水平移动，不做高度上的上下移动
+            dashCDtime = dashCD;
+            dashControll();
+        }
+
+        if (isDash)
+        {
+            this.Dash(); //冲刺
+        }
+        else
+        {
+            this.Moving(); //角色移动-WASD
+        }
+
+        if (dashCDtime>0)
+            dashCDtime = TimeCount(dashCDtime);
+    }
+
+    //移动
+    void Moving()
+    {
+        float speed = moveSpeed;//速度赋值默认速度
+
+        ////跳跃
+        //if (controller.isGrounded) 
+        //{
+        //    if (Input.GetButton("Jump"))
+        //        moveDirection.y = jumpSpeed;
+        //}
+
+        //跑步
+        if (controller.isGrounded &&(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))
+        {
+            speed = runSpeed;
+        }
+        //移动
+        moveDirection.x = Input.GetAxis("Horizontal") * speed;
+        moveDirection.z = Input.GetAxis("Vertical") * speed;
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    //旋转
+    void Rotating()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;//存储射线信息
+        if (Physics.Raycast(ray, out hitInfo, 200, groundLayerIndex))//生成射线
+        {
+            Vector3 playerToMouse = hitInfo.point - transform.position;
+            playerToMouse.y = 0;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            this.transform.rotation = newRotation;
+        }
+    }
+
+
+    //时间倒计时
+    float TimeCount(float timeUse)
+    {
+        timeUse -= Time.deltaTime;
+        return timeUse;
+    }
+
+    //冲刺
+    private void Dash()
+    {
+        if (dashTime < 0)// reset
+        {
+            isDash = false;
+            dashTime = dashDuration;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+            controller.Move(moveDirection * dashSpeed * Time.deltaTime);
+        }
+    }
+
+    //获取系数
+    private void dashControll()
+    {
+        //dashCoefficient = this.GetComponent<>; //获取人物属性脚本中的人物负重并且进行计算
+        dashSpeed *= dashCoefficient;
+        dashDuration *= dashCoefficient;
+    }
+}
