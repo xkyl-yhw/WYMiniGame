289252506d199734f94a3a23712f8fa65a1ddbaf
etihw_lin <etihw_mxlin@163.com>
2020-10-27 00:20:01 +0800

    1、修改开始界面

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+
 public class Login : NetworkBehaviour
 {
     public GameObject loginCanvas;
---
+++ Assets/Scripts/UI/Login/LoginAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginAnimation : MonoBehaviour
+{
+    public Text a;
+    private int c = 1;
+
+    public GameObject canvasFirst;
+    public Button clickToEnter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickToEnter.onClick.AddListener(ChangeToSelection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(a != null)
+        {
+            Vector4 b = a.color;
+            b.w += Time.deltaTime * c;
+            if (b.w>1.5 || b.w<-0.5)
+            {
+                c = -c;
+            }
+            a.color = b;
+        }
+    }
+
+    void ChangeToSelection()
+    {
+        Destroy(canvasFirst);
+    }
+}
